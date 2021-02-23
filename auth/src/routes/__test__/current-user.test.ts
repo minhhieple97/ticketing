2ef54e7d078903ea19f496536b903194a2a33a5c
HiepCongChua <hieplevuc@gmail.com>
2021-02-23 22:01:16 +0700

    complete setup testing

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,11 @@
+import request from "supertest";
+import app from "../../app";
+it("fails when an incorect password is supplied", async () => {
+  const cookie = await global.signin();
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .set("Cookie", cookie)
+    .send()
+    .expect(200);
+  expect(response.body.currentUser.email).toEqual("test@test.com");
+});
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,25 +1,45 @@
-import mongoose from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server'
-import app from '../app';
+import mongoose from "mongoose";
+import request from "supertest";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import app from "../app";
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 let mongo: any;
 beforeAll(async () => {
-    process.env.JWT_KEY = 'secret';
-    mongo = new MongoMemoryServer();
-    const mongoUri = await mongo.getUri();
-    await mongoose.connect(mongoUri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-})
+  process.env.JWT_KEY = "secret";
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
 beforeEach(async () => {
-    const collections = await mongoose.connection.db.collections();
+  const collections = await mongoose.connection.db.collections();
 
-    for (let collection of collections) {
-        await collection.deleteMany({});
-    }
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
 });
 
 afterAll(async () => {
-    await mongo.stop();
-    await mongoose.connection.close();
+  await mongo.stop();
+  await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+  const cookie = response.get("Set-Cookie");
+  return cookie;
+};
