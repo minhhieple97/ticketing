de721398d20ceddcd5a0361e1d4662518dd6540b
HiepCongChua <hieplevuc@gmail.com>
2021-02-25 22:09:09 +0700

    update

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 import app from "./app";
 const start = async () => {
-  // try {
-  //   await mongoose.connect("mongodb://auth-mongo-service:27017/auth", {
-  //     useCreateIndex: true,
-  //     useNewUrlParser: true,
-  //     useUnifiedTopology: true,
-  //   });
-  // } catch (error) {
-  //   console.error(error);
-  // }
+  try {
+    await mongoose.connect("mongodb://auth-mongo-service:27017/auth", {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
+    console.error(error);
+  }
   app.listen(3000, () => {
     console.log("Listening on port 3000.");
   });
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,41 +1,53 @@
-import { useState } from 'react';
-import axios from 'axios';
-import useRequest from '../../hooks/use-request'
-import Router from 'next/router'
+import { useState } from "react";
+import axios from "axios";
+import useRequest from "../../hooks/use-request";
+import Router from "next/router";
 function signup() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const { doRequest, errors } = useRequest({
-        url: '/api/users/signup',
-        method: 'post',
-        body: {
-            email, password
-        },
-        onSuccess: () => Router.push('/')
-    });
-    const onSubmit = async (event) => {
-        try {
-            event.preventDefault();
-            await doRequest()
-        } catch (error) {
-            const message = error.response.data.errors || [{ message: 'Sorry something went wrong' }]
-            setErrors(message)
-        }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push("/"),
+  });
+  const onSubmit = async (event) => {
+    try {
+      event.preventDefault();
+      await doRequest();
+    } catch (error) {
+      const message = error.response.data.errors || [
+        { message: "Sorry something went wrong" },
+      ];
+      setErrors(message);
     }
-    return (
-        <form onSubmit={onSubmit} >
-            <h1>Sign Up</h1>
-            <div className="form-group" >
-                <label>Email Address</label>
-                <input value={email} onChange={e => setEmail(e.target.value)} className="form-control" />
-            </div>
-            <div className="form-group" >
-                <label>Password</label>
-                <input value={password} onChange={e => setEmail(e.target.value)} type="password" className="form-control" />
-            </div>
-            {errors}
-            <button className="btn btn-primary" >Sign Up</button>
-        </form>
-    )
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        />
+      </div>
+      {errors}
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
 }
-export default signup
+export default signup;
