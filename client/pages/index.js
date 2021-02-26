b2e472ee7beaab4c1a5b640eaa759199dc1901a2
minhhieple <hieplevuc@gmail.com>
2021-02-26 17:52:16 +0700

    setup build client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,21 @@
+import axios from 'axios'
+import buildClient from '../api/build-client'
 const LandingPage = ({ color }) => {
   console.log("I am in the component", color)
   return <h2>Landing page</h2>;
 };
-LandingPage.getInitialProps = () => {
-  console.log('I am on the server!');
-  return { color: 'red' }
+LandingPage.getInitialProps = ({ req }) => {
+  if (typeof window === 'undefined') {
+    const { data } = await axios.get(
+      `http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/curreentuser`,
+      {
+        headers: req.headers
+      })
+  }
+  else {
+    const { data } = await axios.get('/api/users/currentuser')
+    return data
+  }
+
 }
 export default LandingPage
