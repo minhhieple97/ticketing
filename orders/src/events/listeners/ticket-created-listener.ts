fb7ec8aa8d6fc43a26676749abafd921b4492093
minhhieple <hieplevuc@gmail.com>
2021-03-12 14:05:38 +0700

    update

---
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,21 @@
+import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming';
+import { Subjects } from '@lmhticket/common';
+import { TicketCreatedEvent } from '@lmhticket/common';
+import { Listener } from '@lmhticket/common';
+import { Ticket } from '../../models/ticket';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent>{
+    readonly subject = Subjects.TicketCreated;
+    queueGroupName = queueGroupName;
+    async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+
+        const { id, title, price } = data;
+        const ticket = Ticket.build({
+            id,
+            title,
+            price
+        });
+        await ticket.save();
+        msg.ack()
+    }
+}
--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose, { mongo } from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 interface TicketAttrs {
     title: string;
@@ -10,6 +11,7 @@ interface TicketDoc extends mongoose.Document {
     title: string;
     price: number;
     userId: string;
+    version: number;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -36,7 +38,8 @@ const ticketSchema = new mongoose.Schema({
         }
     }
 })
-
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin)
 ticketSchema.statics.build = (attr: TicketAttrs) => {
     return new Ticket(attr)
 }
