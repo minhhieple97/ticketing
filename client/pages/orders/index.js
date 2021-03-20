86d6c2483818697ee66f96ff8c41e8f3cc34f520
minhhieple <hieplevuc@gmail.com>
2021-03-20 15:53:18 +0700

    update

--- client/pages/orders/index.js
+++ client/pages/orders/index.js
@@ -1,38 +1,22 @@
 const OrderIndex = ({ orders }) => {
-    return (
-        <ul class="list-group" >
-            {orders.map((order) => {
-                let status = null;
-                switch (order.status) {
-                    case 'created':
-                        status = 'badge-primary'
-                        break;
-                    case 'cancelled':
-                        status = 'badge-dark'
-                        break;
-                    case 'complete':
-                        status = 'badge-success'
-                        break;
-
-                    case 'awaiting:payment':
-                        status = 'badge-warning'
-                        break;
-
-                    default:
-                        status = 'badge-secondary'
-                        break;
-                }
-                return (
-                    <li class="list-group-item d-flex justify-content-between align-items-center" key={order.id}>
-                        {order.ticket.title} - <span class={`badge ${status} badge-pill`}>  {order.status}</span>
-                    </li>
-                );
-            })}
-        </ul>
-    );
+  return (
+    <ul className="list-group list-group-flush">
+      {orders.length > 0 ? (
+        orders.map((order) => {
+          return (
+            <li key={order.id} className="list-group-item">
+              {order.ticket.title} - {order.status}
+            </li>
+          );
+        })
+      ) : (
+        <h2 style={{ textAlign: "center" }}>No order found</h2>
+      )}
+    </ul>
+  );
 };
 OrderIndex.getInitialProps = async (context, client) => {
-    const { data } = await client.get('/api/orders');
-    return { orders: data };
+  const { data } = await client.get("/api/orders");
+  return { orders: data };
 };
 export default OrderIndex;
--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,51 +1,52 @@
-import { validatorRequest } from '@lmhticket/common';
-import { NotAuthorized, OrderStatus, BadRequestError } from '@lmhticket/common';
-import { NotFoundError } from '@lmhticket/common';
-import { body } from 'express-validator';
-import { requireAuth } from '@lmhticket/common';
-import express, { Request, Response } from 'express';
-import { Order } from '../models/order';
-import { stripe } from '../stripe';
-import { Payment } from '../models/payment';
-import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
-import { natsWrapper } from '../nats-wrapper';
+import { validatorRequest } from "@lmhticket/common";
+import { NotAuthorized, OrderStatus, BadRequestError } from "@lmhticket/common";
+import { NotFoundError } from "@lmhticket/common";
+import { body } from "express-validator";
+import { requireAuth } from "@lmhticket/common";
+import express, { Request, Response } from "express";
+import { Order } from "../models/order";
+import { stripe } from "../stripe";
+import { Payment } from "../models/payment";
+import { PaymentCreatedPublisher } from "../events/publishers/payment-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 const router = express.Router();
 router.post(
-    '/api/payments',
-    requireAuth,
-    [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
-    validatorRequest,
-    async (req: Request, res: Response) => {
-        const { orderId, token } = req.body;
+  "/api/payments",
+  requireAuth,
+  [body("token").not().isEmpty(), body("orderId").not().isEmpty()],
+  validatorRequest,
+  async (req: Request, res: Response) => {
+    const { orderId, token } = req.body;
 
-        const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId);
 
-        if (!order) {
-            throw new NotFoundError();
-        }
-        if (order.userId !== req.currentUser!.id) {
-            throw new NotAuthorized();
-        }
-        if (order.status === OrderStatus.Cancelled) {
-            throw new BadRequestError('Cannot pay for an cancelled order');
-        }
-        const charge = await stripe.charges.create({
-            currency: 'usd',
-            amount: order.price * 100,
-            source: token,
-        });
-        const payment = Payment.build({
-            orderId,
-            stripeId: charge.id
-        });
-        await payment.save()
-        new PaymentCreatedPublisher(natsWrapper.client).publish({
-            id: payment.id,
-            orderId: payment.orderId,
-            stripeId: payment.stripeId
-        })
-        res.status(201).send({ id: payment.id });
+    if (!order) {
+      throw new NotFoundError();
     }
+    console.log({ order }, req.currentUser);
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorized();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError("Cannot pay for an cancelled order");
+    }
+    const charge = await stripe.charges.create({
+      currency: "usd",
+      amount: order.price * 100,
+      source: token,
+    });
+    const payment = Payment.build({
+      orderId,
+      stripeId: charge.id,
+    });
+    await payment.save();
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId,
+    });
+    res.status(201).send({ id: payment.id });
+  }
 );
 
-export { router as createChargeRouter }
+export { router as createChargeRouter };
